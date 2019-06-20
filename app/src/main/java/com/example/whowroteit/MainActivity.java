604e55866d6a887e8e6875d11d525e76b0163ed7
Navjot Singh <navjotd139@gmail.com>
2019-06-20 12:51:22 +0530

    network connectivity and valid input is ensured

--- app/src/main/java/com/example/whowroteit/MainActivity.java
+++ app/src/main/java/com/example/whowroteit/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.whowroteit;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -25,5 +29,34 @@ public class MainActivity extends AppCompatActivity {
     public void searchBooks(View view) {
 
         String book = BookInput.getText().toString();
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputMethodManager != null) {
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if(connMgr != null){
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+
+
+        if(networkInfo != null && networkInfo.isConnected() && book.length() != 0) {
+            new FetchBook(Author, Title).execute(book);
+            Author.setText("");
+            Title.setText(R.string.loading);
+        }
+        else{
+            if(book.length() == 0){
+                Author.setText("");
+                Title.setText(R.string.no_search_term);
+            }
+            else{
+                Author.setText("");
+                Title.setText(R.string.no_network);
+            }
+        }
+
     }
 }
