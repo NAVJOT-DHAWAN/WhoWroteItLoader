03463e6b0be0c8a148c298faadb3bda4d959c695
Navjot Singh <navjotd139@gmail.com>
2019-06-20 12:51:48 +0530

    AsyncTask methods modified

--- app/src/main/java/com/example/whowroteit/FetchBook.java
+++ app/src/main/java/com/example/whowroteit/FetchBook.java
@@ -3,9 +3,15 @@ package com.example.whowroteit;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.ref.WeakReference;
 
 public class FetchBook extends AsyncTask<String,Void,String> {
+
+
 
     private WeakReference<TextView> Author;
     private WeakReference<TextView> Title;
@@ -17,11 +23,47 @@ public class FetchBook extends AsyncTask<String,Void,String> {
 
     @Override
     protected String doInBackground(String... strings) {
-        return null;
+
+        return NetworkUtils.getBookInfo(strings[0]);
+
     }
 
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+        int i = 0;
+        String title = null;
+        String authors = null;
+        try{
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            while (i<itemsArray.length()&&(authors == null && title == null)){
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                try{
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                i++;
+            }
+            if(title != null && authors != null){
+                Title.get().setText(title);
+                Author.get().setText(authors);
+            }
+            else
+            {
+                Title.get().setText(R.string.no_results);
+                Author.get().setText("");
+            }
+
+        }catch (JSONException e){
+            Title.get().setText(R.string.no_results);
+            Author.get().setText("");
+            e.printStackTrace();
+        }
     }
 }
